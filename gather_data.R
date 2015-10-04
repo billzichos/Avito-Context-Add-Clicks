225fba78bcf74e9818141b25f67cb4b5e8beccbe
billzichos <billzichos@hotmail.com>
2015-10-04 16:14:15 -0400

    gather data cleanup

--- gather_data.R
+++ gather_data.R
@@ -19,6 +19,7 @@ setwd(wd)
 # the scripts below seem to corrupt the 7z files.
 
 source("~/GitHub/Get-Raw-Data/download.R")
+
 downloadKaggle("avito-context-ad-clicks","UserInfo.tsv.7z")
 downloadKaggle("avito-context-ad-clicks","Category.tsv.7z")
 downloadKaggle("avito-context-ad-clicks","Location.tsv.7z")
@@ -33,16 +34,3 @@ downloadKaggle("avito-context-ad-clicks","SearchInfo.tsv.7z")
 downloadKaggle("avito-context-ad-clicks","database.sqlite.7z")
 
 # at this point manually extract the 7z zip files
-
-trnFile <- "train.csv"
-tstFile <- "test.csv"
-
-download.file(trnUrl, paste(wd,"\\train.csv", sep=""), quiet=TRUE)
-download.file(tstUrl, paste(wd,"\\test.csv", sep=""), quiet=TRUE)
-
-datalist <- list(
-        train=read.csv(
-                paste(wd,"\\",trnFile, sep=""), header=TRUE, as.is=TRUE), 
-        test=read.csv(
-                paste(wd,"\\",tstFile, sep=""), header=TRUE, as.is=TRUE)
-        )
